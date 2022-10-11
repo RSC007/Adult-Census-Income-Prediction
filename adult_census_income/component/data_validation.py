9e9d71aa37af93f838e5ff1254cad097b8e649d2
RSC007 <rushichitte1998@gmail.com>
2022-10-11 11:07:41 +0530

    data validation is completed

--- adult_census_income/component/data_validation.py
+++ adult_census_income/component/data_validation.py
@@ -1,8 +1,15 @@
+import json
 import os
 import sys
 import numpy as np
+import pandas as pd
+from evidently import dashboard
+from evidently.model_profile import Profile
+from evidently.model_profile.sections import DataDriftProfileSection
+from evidently.dashboard import Dashboard
+from evidently.dashboard.tabs import DataDriftTab
 
-from adult_census_income.entity.artifact_entity import DataIngestionArtifact
+from adult_census_income.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from adult_census_income.entity.entity_config import DataValidationConfig
 from adult_census_income.exception import AdutlCensusIncomeException
 from adult_census_income.constant import *
@@ -43,7 +50,7 @@ class DataValidation:
             df = dataframe.copy()
             not_need_columns = ["education", "fnlwgt", "marital-status", "capital-gain", "capital-loss"]
             df.drop(not_need_columns, axis=1, inplace=True)
-            logging.info(f"Remove not need colimns: [{not_need_columns}]")
+            logging.info(f"Remove not need columns: [{not_need_columns}]")
             return df
 
         except Exception as e:
@@ -67,9 +74,76 @@ class DataValidation:
         except Exception as e:
             raise AdutlCensusIncomeException(e, sys) from e
 
+    def get_train_and_test_df(self):
+        try:
+            train_df = pd.read_csv(self.data_ingestion_artifact_config.train_file_path)
+            test_df = pd.read_csv(self.data_ingestion_artifact_config.test_file_path)
+
+            return train_df, test_df
+        except Exception as e:
+            raise AdutlCensusIncomeException(e, sys) from e
+
+    def get_and_save_data_drift_report(self):
+        try:
+            self.is_train_test_file_exists()
+            self.validate_dataset_schema()
+
+            profile = Profile(sections=[DataDriftProfileSection()])
+
+            train_df, test_df = self.get_train_and_test_df()
+
+            profile.calculate(train_df, test_df)
+
+            report = json.loads(profile.json())
+
+            report_file_path = self.data_validaiton_config.report_file_path
+
+            report_dir = os.path.dirname(report_file_path)
+
+            os.makedirs(report_dir, exist_ok=True)
+
+            with open(report_file_path, 'w') as report_file:
+                json.dump(report, report_file, indent=6)
+
+            return report
+        except Exception as e:
+            raise AdutlCensusIncomeException(e, sys) from e
+
+    def save_data_drift_report_page(self):
+        try:
+            dashboard = Dashboard(tabs=[DataDriftTab()])
+            train_df, test_df = self.get_train_and_test_df()
+            dashboard.calculate(train_df, test_df)
+
+            report_page_file_path = self.data_validaiton_config.report_page_file_path
+            report_page_dir = os.path.dirname(report_page_file_path)
+            os.makedirs(report_page_dir, exist_ok=True)
+
+            dashboard.save(report_page_file_path)
+        except Exception as e:
+            raise AdutlCensusIncomeException(e, sys) from e
+
+    def is_data_drift_found(self):
+        try:
+            report = self.get_and_save_data_drift_report()
+            self.save_data_drift_report_page()
+            return True
+        except Exception as e:
+            raise AdutlCensusIncomeException(e, sys) from e
+
     def initiate_data_validation(self):
         try:
             self.is_train_test_file_exists()
             self.validate_dataset_schema()
+            self.is_data_drift_found()
+
+            data_validation_artifact = DataValidationArtifact(
+                schema_file_path=self.data_validaiton_config.schema_file_path,
+                report_file_path=self.data_validaiton_config.report_file_path,
+                report_page_file_path=self.data_validaiton_config.report_page_file_path,
+                is_validated=True,
+                message="Data validation perform successfully"
+            )
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
         except Exception as e:
             raise AdutlCensusIncomeException(e, sys) from e
--- adult_census_income/entity/artifact_entity.py
+++ adult_census_income/entity/artifact_entity.py
@@ -5,4 +5,4 @@ DataIngestionArtifact = namedtuple("DataIngestionArtifact",
                                    ["train_file_path", "test_file_path", "is_ingested", "message"])
 
 
-DataValidationArtifact = namedtuple("DataValidationArtifact", [])
+DataValidationArtifact = namedtuple("DataValidationArtifact", ["schema_file_path", "report_file_path", "report_page_file_path", "is_validated", "message"])
